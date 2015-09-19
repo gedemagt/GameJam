fec924e4bcb4ba6e2542e1a33daf93730a3b5181
Anders <duemoselund@gmail.com>
2015-09-20 00:23:48 +0200

    Updated distortion, added cheering crowd

--- Assets/Scripts/EnvironmentController.cs
+++ Assets/Scripts/EnvironmentController.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class EnvironmentController : MonoBehaviour {
     public GameObject background;
     public float rotateionCounter = 0;
+    public GameObject crowd;
+    public AudioClip cheering;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +16,21 @@ public class EnvironmentController : MonoBehaviour {
 	void Update () {
         background.transform.rotation = Quaternion.Euler(new Vector3(background.transform.rotation.x, background.transform.rotation.y, background.transform.rotation.z + rotateionCounter));
         rotateionCounter += 0.1f;
-	}
+        if (Input.GetMouseButtonDown(0)) UpDown(true);
+        if (Input.GetMouseButtonDown(1)) UpDown(false);
+    }
+
+    void UpDown(bool up) {
+        if (up) {
+            iTween.MoveBy(crowd, new Vector3(0f, 3f, 0), 1);
+            gameObject.GetComponent<AudioSource>().clip = cheering;
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        if (!up) {
+            iTween.MoveBy(crowd, new Vector3(0f, -3f, 0), 1);
+            gameObject.GetComponent<AudioSource>().Stop();
+        }
+
+
+    }
 }
