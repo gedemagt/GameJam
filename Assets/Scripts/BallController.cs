f079a71d91552b3fdeb12f959146f5e03d970ac3
Anders <duemoselund@gmail.com>
2015-09-19 22:22:56 +0200

    Plenty of acid stuff

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour {
 	Rigidbody rb;
     public Vector3 StartVelocity;
     public Paddle startPaddle;
-
+    public Sprite[] catBall;
+    public Image imageSprite;
+    private int catAniCount = 0;
 	// Use this for initialization
 	void Start () {
         startPaddle.Attach(this);
@@ -15,8 +18,14 @@ public class BallController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        AnimateCat();
+    }
 
-	}
+    void AnimateCat() {
+        imageSprite.sprite = catBall[catAniCount];
+        catAniCount++;
+        if (catAniCount >= 12) catAniCount = 0;
+    }
 
 	public float bounciness;
 	private Vector3 lastVelocity;
@@ -60,6 +69,8 @@ public class BallController : MonoBehaviour {
             outVelocity += Vector3.left * paddle.GetVelocity();
         }
 		rb.velocity = outVelocity;
-	}
+        if (outVelocity.x > 0) imageSprite.transform.rotation = Quaternion.Euler(new Vector3(0, -180, 0));
+        else if (outVelocity.x < 0) imageSprite.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+    }
 
 }
---
+++ Assets/Scripts/EnvironmentController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnvironmentController : MonoBehaviour {
+    public GameObject background;
+    public float rotateionCounter = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        background.transform.rotation = Quaternion.Euler(new Vector3(background.transform.rotation.x, background.transform.rotation.y, background.transform.rotation.z + rotateionCounter) * Time.deltaTime);
+        rotateionCounter += 0.1f;
+	}
+}
