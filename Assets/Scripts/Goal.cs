d1124adf412e2a8d0daeb4ee5324491df7ce305e
Jesper <gedemagt@gmail.com>
2015-09-19 12:05:26 +0200

    Added goals and stuff

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour {
+
+    int Count = 0;
+
+	// Use this for initialization
+	void Start () {
+        Count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public int GetCount() { return Count; }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Count++;
+        collision.gameObject.GetComponent<BallManager>().Reset();
+    }
+}
