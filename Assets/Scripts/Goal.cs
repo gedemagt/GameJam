cfca19d3128701ed2e996a964e3ff4e7977986ed
Jesper <gedemagt@gmail.com>
2015-09-19 20:12:17 +0200

    Updated game progression

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -4,25 +4,21 @@ using System.Collections;
 
 public class Goal : MonoBehaviour {
 
-    int Count = 0;
-    public Text score;
     public Paddle left;
+    public delegate void OnCount();
+    public OnCount onCount;
 
 	// Use this for initialization
 	void Start () {
-        Count = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        score.text = Count.ToString();
 	}
-
-    public int GetCount() { return Count; }
 
     void OnCollisionEnter(Collision collision)
     {
-        Count++;
         left.Attach(collision.gameObject.GetComponent<BallController>());
+        onCount();
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -6,17 +7,28 @@ public class PlayerController : MonoBehaviour {
     public QFrame frame;
     public Paddle paddle;
     public Goal goal;
-
+    public Text text;
+    private int count = 0;
+    public int level1 = 5;
+    public int level2 = 8;
 
 	// Use this for initialization
 	void Start () {
+
         frame.setLevel("id2");
         paddle.scaleAccordingToVariance = false;
+        goal.onCount += OnCount;
 	}
+
+    private void OnCount()
+    {
+        count++;
+        text.text = count.ToString();
+        if (count == level1) frame.setLevel("id1");
+        if (count == level2) paddle.scaleAccordingToVariance = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (goal.GetCount() > 5) frame.setLevel("id1");
-        if (goal.GetCount() > 8) paddle.scaleAccordingToVariance = true;
 	}
 }
